fd86133afe5f0f93ed142fe40560c457ad707772
chris <ajackal244@gmail.com>
2018-07-25 15:41:41 -0400

    update package requirements with requests

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from setuptools import setup
 
 setup(
     name='honeyhornet',
-    version='1.0',
+    version='1.1',
     packages=['honeyhornet'],
     url='https://github.com/ajackal/honey-hornet.git',
     license='GNU General Public License v3.0',
     author='Chris Miller',
     author_email='ajackal244@gmail.com',
     description='port scanner & credential tester',
-    install_requires=['python-nmap', 'termcolor', 'PyYAML', 'pexpect']
+    install_requires=['python-nmap', 'termcolor', 'PyYAML', 'pexpect', 'requests']
 )
