fb8dc82b8231882b4c13ac46c17610c86c6eb994
cmill <ajackal244@gmail.com>
2018-09-11 17:57:32 -0400

    updated corescanner docstrings, added some bool returns

--- honeyhornet/corescanner.py
+++ honeyhornet/corescanner.py
@@ -14,19 +14,18 @@ import buildconfig
 
 
 class HoneyHornet(HoneyHornetLogger):
-    """
-    Uses NMAP to scan the targets and ports listed in the configuration file.
+    """Uses NMAP to scan the targets and ports listed in the configuration file.
 
-    Inherits HoneyHornet Logger for all the logging functionality.
+    Inherits HoneyHornetLogger for all the logging functionality.
 
     Attributes:
-        - vulnerable_hosts(list) = list of all the vulnerable hosts found during the scan
-        - live_hosts(list) = all live hosts that are found
-        - time_stamp(str) = used for creating file names
-        - verbose(boolean) = set to True for more verbose print statements through execution
-        - default_filepath(str) = gets the current working directory to generate absolute file paths
-        - default_config_filepath(str) = the default file path to the config file
-        - config(dict) = YAML configuration gets loaded here
+        vulnerable_hosts (list): list of all the vulnerable hosts found during the scan
+        live_hosts (list): all live hosts that are found
+        time_stamp (str): used for creating file names
+        verbose (bool): set to True for more verbose print statements through execution
+        default_filepath (str): gets the current working directory to generate absolute file paths
+        default_config_filepath (str): the default file path to the config file
+        config (dict): YAML configuration gets loaded here
     """
 
     vulnerable_hosts = []
@@ -42,101 +41,112 @@ class HoneyHornet(HoneyHornetLogger):
 
     def load_configuration_file(self, yml_config):
         """Loads the YAML configuration file needed to run the program.
-
         Use command line option "--config" to load a specific file.
         Will load "config.yml" in the "configs/" directory by default.
 
         Args:
-            - yml_config(str): the YAML configuration file you want to load.
+            yml_config (str): the YAML configuration file you want to load.
 
         Returns:
-            - config(dict): variable containing all the configuration settings.
+            str: Successfully loaded or built configuration.
         """
         try:
             with open(yml_config, 'r') as cfg_file:
                 self.config = yaml.load(cfg_file)
+                return "Successfully loaded configuration file."
         except IOError:
             b = buildconfig.BuildConfig()
             self.load_configuration_file(self.default_config_filepath)
+            return "Successfully built configuration."
 
     def write_results_to_csv(self):
         """Writes the results of the scan to a CSV formatted file.
 
-        Args:
-            - None
-
         Returns:
-            - Nothing
+            bool: True for success, False otherwise.
         """
-        results_file = self.time_stamp + "_recovered_passwords.csv"
-        results_file = os.path.join(self.default_filepath, "reports", results_file)
-        headers = "Time Stamp,IP Address,Service,Port,Username,Password\n"
-        with open(results_file, 'a') as open_csv:
-            open_csv.write(headers)
-            for host in self.vulnerable_hosts:
-                host.get_credentials(open_csv)
+        try:
+            results_file = self.time_stamp + "_recovered_passwords.csv"
+            results_file = os.path.join(self.default_filepath, "reports", results_file)
+            headers = "Time Stamp,IP Address,Service,Port,Username,Password\n"
+            with open(results_file, 'a') as open_csv:
+                open_csv.write(headers)
+                for host in self.vulnerable_hosts:
+                    host.get_credentials(open_csv)
+            return True
+        except IOError:
+            logging.error("Error writing to CSV file.")
+            return False
 
     def write_results_to_json(self):
         """Writes the results of the scan to a JSON formatted file.
 
-        Args:
-            - None
-
         Returns:
-            - Nothing
+            bool: True for success, False otherwise.
         """
-        results_file = self.time_stamp + "_saved_objects.json"
-        results_file = os.path.join(self.default_filepath, "saves", results_file)
-        with open(results_file, 'a') as open_json_file:
-            for host in self.vulnerable_hosts:
-                to_json = {'host': host.ip, 'ports': host.ports, 'credentials': host.credentials}
-                open_json_file.write(json.dumps(to_json))
-                open_json_file.write("\n")
+        try:
+            results_file = self.time_stamp + "_saved_objects.json"
+            results_file = os.path.join(self.default_filepath, "saves", results_file)
+            with open(results_file, 'a') as open_json_file:
+                for host in self.vulnerable_hosts:
+                    to_json = {'host': host.ip, 'ports': host.ports, 'credentials': host.credentials}
+                    open_json_file.write(json.dumps(to_json))
+                    open_json_file.write("\n")
+            return True
+        except IOError:
+            logging.error("Error writing to JSON file.")
+            return False
 
     def log_open_port(self, host, port, status):
         """ Logs any host with an open port to a custom formatted log file.
 
         Args:
-            - host(str) = IP Address of the host with an open port
-            - port(str) = the open port that was found
-            - status(str) = always 'open'
+            host (str): IP Address of the host with an open port
+            port (str): the open port that was found
+            status (str): always 'open'
 
         Returns:
-            - Nothing
-
+            bool: True for success, False otherwise.
         """
-        logfile_name = str(date.today()) + "_open_ports.log"
-        logfile_name = os.path.join(self.default_filepath, "logs", logfile_name)
-        event = " host={0}   \tport={1}  \tstatus={2}".format(colored(host, "green"),
-                                                              colored(port, "green"),
-                                                              colored(status, "green"))
-        if self.verbose:
-            print("[*] Open port found:{0}".format(event))
-        self.write_log_file(logfile_name, event)
-        self.write_log_file(logfile_name, "\n")
+        try:
+            logfile_name = str(date.today()) + "_open_ports.log"
+            logfile_name = os.path.join(self.default_filepath, "logs", logfile_name)
+            event = " host={0}\tport={1}\tstatus={2}".format(colored(host, "green"), colored(port, "green"),
+                                                             colored(status, "green"))
+            if self.verbose:
+                print("[*] Open port found:{0}".format(event))
+            self.write_log_file(logfile_name, event)
+            self.write_log_file(logfile_name, "\n")
+            return True
+        except IOError:
+            logging.error("Error writing to log file.")
+            return False
 
     def calculate_total_number_of_hosts(self, target_list):
         """ Calculates the total number of hosts that will be scanned.
 
         Args:
-            - target_list(list) = a single item list returned from loading the YAML configuration
+            target_list (list): a single item list returned from loading the YAML configuration
 
         Returns:
-            - total(int) = the total number of hosts in the target list
+            total(int): the total number of hosts in the target list if successful, -1 if an exception is thrown.
         """
-        target_list = str(target_list).strip("['']")
-        target_list = os.path.join(self.default_filepath, "targets", target_list)
-        with open(target_list, 'r') as open_target_list:
-            return len(open_target_list.readlines())
+        try:
+            target_list = str(target_list).strip("['']")
+            target_list = os.path.join(self.default_filepath, "targets", target_list)
+            with open(target_list, 'r') as open_target_list:
+                return len(open_target_list.readlines())
+        except IOError:
+            return -1
 
     def calculate_number_of_hosts(self, target_list):
         """ Calculates the number of a live hosts and the open percentage.
 
         Args:
-            - target_list(list) = a single item list returned from loading the YAML configuration
+            target_list (list): a single item list returned from loading the YAML configuration
 
         Returns:
-            - writes to results to a log file and prints to screen
+            bool: True for success, False otherwise.
         """
         try:
             total = self.calculate_total_number_of_hosts(target_list)
@@ -150,33 +160,49 @@ class HoneyHornet(HoneyHornetLogger):
                 log_file.write(new_log)
                 log_totals = "{0}\{1} = {2}%\n".format(live, total, round(percentage, 2))
                 log_file.write(log_totals)
+            return True
         except Exception as error:
             logging.exception("calculate_number_of_hosts\t{0}".format(error))
+            return False
 
     def create_new_vulnerable_host(self, host, ports):
-        """ Instantiates a new object of the Vulnerable host """
-        new_host = VulnerableHost(host[0])  # creates new object
-        self.vulnerable_hosts.append(new_host)
-        for port in ports:
-            port_state = port[1]['state']  # defines port state variable
-            if port_state == 'open':  # checks to see if status is open
-                new_host.add_vulnerable_port(port[0])
-                self.log_open_port(host[0], port[0], port_state)
+        """ Instantiates a new object of the Vulnerable host
+
+        Args:
+            host (list): list containing the host IP address
+            ports (list): list containing the open ports found
+
+        Returns:
+            bool: True if success, False otherwise.
+
+        """
+        try:
+            new_host = VulnerableHost(host[0])  # creates new object
+            self.vulnerable_hosts.append(new_host)
+            for port in ports:
+                port_state = port[1]['state']  # defines port state variable
+                if port_state == 'open':  # checks to see if status is open
+                    new_host.add_vulnerable_port(port[0])
+                    self.log_open_port(host[0], port[0], port_state)
+            return True
+        except Exception:
+            logging.exception("Error creating new vulnerable host.")
+            return False
 
     def check_admin_ports(self, target_list, ports_to_scan):
         """Scans for a live host and for any open common admin ports defined in the configuration file.
         If an open port is found, it instantiates a class for that host and records all the open ports.
 
-        Tests all live host for open 'admin' ports
-
-        Changed to let NMAP handling the threading. Had implemented threading, but threading NMAP,
-        which is threaded seems to add complications. New implementation is very fast and almost no issues.
-
         Ports list as argument needed to have the whitespace stripped between each port, otherwise the NMAP command
         is not constructed properly.
-        """
 
-        service = "admin_port_scanner"
+        Args:
+            target_list (str): file name of the target list to be used.
+            ports_to_scan (list): list of ports to scan.
+
+        Returns:
+            bool: True for success, False otherwise.
+        """
         try:
             scanner = nmap.PortScannerYield()  # defines port scanner function
             print("[*] checking for open admin ports...")
@@ -197,13 +223,15 @@ class HoneyHornet(HoneyHornetLogger):
                     ports = host[1]['scan'][host[0]]['tcp'].viewitems()  # retrieves tcp port results from scan
                     self.create_new_vulnerable_host(host, ports)
                 except KeyError:
-                    continue
+                    return False
+            return True
         # except scanner.PortScannerError as error:
         #     print "[!] Error running port scanner, check target list path."
         #     logging.exception("{0}\t{1}".format(service, error))
         #     exit(0)
         except Exception as error:
-            logging.exception("{0}\t{1}".format(service, error))
+            logging.exception("Port Scanner threw the following: {0}".format(error))
+            return False
         except KeyboardInterrupt:
             exit(0)
 
@@ -211,38 +239,77 @@ class HoneyHornet(HoneyHornetLogger):
 class VulnerableHost(HoneyHornet):
     """ Defines a class for each live host with an open admin port and saves the open ports to a list. These objects
      are then used when the CredentialChecker() class is instantiated and services checked.
+
+     Args:
+         ipaddr (str): the IP address of the vulnerable host
+
+     Attributes:
+         ports (list): a list of open ports on the vulnerable host.
+         credentials (list): a list of credentials (dict) that is recovered.
+         banner (list): a list of banner grabs (dict) that is recovered.
+         ip (str): the IP address of the vulnerable host
      """
     def __init__(self, ipaddr):
         HoneyHornet.__init__(self)
         self.ports = []
-        self.credentials = {}
+        self.credentials = []
         self.banner = []
         self.ip = ipaddr
 
     def add_vulnerable_port(self, port):
-        """ Function appends open admin port to list. """
+        """ Function appends open admin port to list.
+
+        Args:
+            port (str): the open port that was found.
+        """
         self.ports.append(port)
 
     def put_credentials(self, service, port, user, password):
-        """ Records credentials of a successful login attempt to an open admin port. """
+        """ Records credentials of a successful login attempt to an open admin port.
+
+        Args:
+            service (str): service used to recover the credential.
+            port (str): the port recovered.
+            user (str): the username recovered.
+            password (str): the password recovered.
+        """
         new_credentials = {"user": user, "password": password, "port": port, "service": service}
-        self.credentials.update(new_credentials)
+        self.credentials.append(new_credentials)
 
     def put_banner(self, port, banner_txt, status, reason, headers):
         """ Adds port, banner to banner list of a port that is defined in the http_ports list or is not handled
          by another service check.
+
+         Args:
+             port (str): the port the banner was recovered from.
+             banner_txt (str): the banner text that was recovered.
+             status (str): the http status code returned.
+             reason (str): the http reason returned.
+             headers (str): the headers returned by the server.
          """
-        self.banner.append(':{0} {1} {2} {3}\n{4}\n'.format(port, status, reason, banner_txt, headers))
+        banner = {"port": port, "status": status, "reason": reason, "banner": banner_txt, "headers": headers}
+        self.banner.append(banner)
 
     def get_credentials(self, open_csv):
-        """ Formats and writes recovered credentials to a CSV file. """
+        """ Formats and writes recovered credentials to a CSV file.
+        Args:
+            open_csv (object): the open file object.
+
+        Returns:
+            bool: True for success, False otherwise.
+        """
         x = len(self.credentials)
-        for credential in self.credentials:
-            open_csv.write("{0},{1},{2},{3},{4},{5}\n".format(self.time_stamp, self.ip,
-                                                              self.credentials['service'],
-                                                              self.credentials['port'],
-                                                              self.credentials['user'],
-                                                              self.credentials['password']))
+        try:
+            for credential in self.credentials:
+                open_csv.write("{0},{1},{2},{3},{4},{5}\n".format(self.time_stamp, self.ip,
+                                                                  self.credentials['service'],
+                                                                  self.credentials['port'],
+                                                                  self.credentials['user'],
+                                                                  self.credentials['password']))
+            return True
+        except IOError:
+            logging.error("Error writing to CSV file (get_credentials).")
+            return False
 
 
 def main():
