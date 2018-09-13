df61537622cf4119353032f08462f9eea40e0321
chris work hp <ajacjal244@gmail.com>
2018-09-13 17:26:15 -0400

    updated logger documentation, cleaned up attributes

--- honeyhornet/logger.py
+++ honeyhornet/logger.py
@@ -4,20 +4,32 @@ from datetime import date, datetime
 
 
 class HoneyHornetLogger:
+    """"Logging class that handles all logging for the entire application.
+
+    Attributes:
+        log_directory (str): the full file path of the logging directory.
+        log_filename (str): the file name for the log file.
+        log_name (str): full file path and file name of the log file.
+    """
     def __init__(self):
         """ Setup logging file path and formatting """
-        self.default_filepath = os.path.dirname(os.getcwd())
-        log_name = str(date.today()) + "_DEBUG.log"
-        log_name = os.path.join(self.default_filepath, "logs", log_name)
-        log_directory = os.path.dirname(log_name)
-        if not os.path.exists(log_directory):
-            os.mkdir(log_directory)
-        logging.basicConfig(filename=log_name, format='%(asctime)s %(levelname)s: %(message)s',
+        self.log_directory = os.path.join(os.path.dirname(os.getcwd()), "logs")
+        self.log_filename = str(date.today()) + "_DEBUG.log"
+        self.log_name = os.path.join(self.log_directory, self.log_filename)
+        # self.log_directory = os.path.dirname(log_name)
+        if not os.path.exists(self.log_directory):
+            os.mkdir(self.log_directory)
+        logging.basicConfig(filename=self.log_filename, format='%(asctime)s %(levelname)s: %(message)s',
                             level=logging.DEBUG)
 
     @staticmethod
     def write_log_file(logfile_name, event):
-        """ Writes the event to the proper log file """
+        """ Writes the event to the proper log file.
+
+         Args:
+             logfile_name (str): the name of the log file the event should be written to.
+             event (str): the event to be written to the log file
+        """
         time_now = datetime.now()
         with open(logfile_name, 'a') as log_file:
             if "\n" not in event:
