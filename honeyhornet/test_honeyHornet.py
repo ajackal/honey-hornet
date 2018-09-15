4399ae9ed9d2039933fca63946e23e3e88dca79d
cmill <ajackal244@gmail.com>
2018-09-14 20:30:02 -0400

    added calculate total/live hosts unit test.

--- honeyhornet/test_honeyHornet.py
+++ honeyhornet/test_honeyHornet.py
@@ -8,29 +8,38 @@ class TestHoneyHornet(TestCase):
         # will test the building of a config using buildconfig
         cs = corescanner.HoneyHornet()
         yml_config = "aNon-existentYAMLconfig.yml"
-        yml_config = "/home/Chris/Projects/honey-hornet/configs/config.yml"
+        real_yml_config = "/home/Chris/Projects/honey-hornet/configs/config.yml"
         expected = "built"
         actual = cs.load_configuration_file(yml_config)
         self.assertIn(expected, actual)
         # self.fail()
 
-    def test_write_results_to_csv(self):
-        self.fail()
+    # def test_write_results_to_csv(self):
+    #     self.fail()
 
-    def test_write_results_to_json(self):
-        self.fail()
+    # def test_write_results_to_json(self):
+    #     self.fail()
 
-    def test_log_open_port(self):
-        self.fail()
+    # def test_log_open_port(self):
+    #     self.fail()
 
     def test_calculate_total_number_of_hosts(self):
-        self.fail()
+        cs = corescanner.HoneyHornet()
+        test_target_list = ['test_target_list.txt']
+        result = cs.calculate_number_of_hosts(test_target_list)
+        self.assertTrue(result)
+        # self.fail()
 
     def test_calculate_number_of_hosts(self):
-        self.fail()
+        cs = corescanner.HoneyHornet()
+        test_target_list = ['test_target_list.txt']
+        actual = cs.calculate_total_number_of_hosts(test_target_list)
+        expected = 5
+        self.assertIs(actual, expected)
+        # self.fail()
 
-    def test_create_new_vulnerable_host(self):
-        self.fail()
+    # def test_create_new_vulnerable_host(self):
+    #     self.fail()
 
-    def test_check_admin_ports(self):
-        self.fail()
+    # def test_check_admin_ports(self):
+    #     self.fail()
