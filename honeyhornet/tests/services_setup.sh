2aefc118d18b48c0527e488c62d7e4b3c0d65009
chrism <ajackal244@gmail.com>
2018-09-27 12:42:44 -0400

    changed depend. for telnet

--- honeyhornet/tests/services_setup.sh
+++ honeyhornet/tests/services_setup.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 if [ $(id -u) -eq 0 ]; then
-    apt install vsftpd openssh-server telnetd
+    apt install vsftpd openssh-server xinetd telnetd
     useradd -m -p 38CSE0qj7d1xE devtestuser
     echo "### Credentials OK ###\n### Login Successful ###\n" > sudo tee /etc/motd
     service vsftpd restart
     service ssh restart
-    service inetd restart
+    service xinetd restart
     python3 honeyhornet/tests/test_http_server.py
 else
     echo "Not running as root!"
